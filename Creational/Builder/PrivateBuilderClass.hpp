ec974551f45aad884a8fe92836f4875f8e24a5d6
stefancretu27 <stefancretu27@gmail.com>
2023-08-07 13:36:24 +0300

    Update PrivateBuilderClass.hpp with implementation that uses a static GetBuilder method, returning a locally constructed builder object, avoiding circular dependency

--- Creational/Builder/PrivateBuilderClass.hpp
+++ Creational/Builder/PrivateBuilderClass.hpp
@@ -1,13 +1,26 @@
 #pragma once 
 
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <iostream>
+
 /*
-* Create the PrivateBuilder class as a private class for the Built class. Then, make the AppendItem method private, in the Built class.
-* Thus, the PrivateBuilder's AddItem that wrapps a call to AppendItem will be the only way to construct the object in phases, enforcing hereby
-* the usage of the PrivateBuilder instance.
-* Therefater, as the PrivateBuilder is tied to the Built object, it gets the *this pointer as argument upon creation. The pointer is used for calling the
-* AppendItem, as the PrivateBuilder must know to which object the new itesm must be added, hence the need for coupling the 2 classes. each Built object
-* has a PrivateBuilder instance, that is constructed from this pointer when Built c-tor is called, which applied to each c-tor overload.
-* As teher is a direct dependency between each Built and PrivateBuilder insatnce, the singleton patter cannot be applied to PrivateBuilder.
+* The below implementation aims at not exposing the Builder's implementation, whereas making possible to construct the Built object in phases
+* only by using the Builder. This is possible as the Built class uses a GetBuilder() static public method for retrieveing the Builder instance
+* which is default created and returned as an anonymous object. Thus, each time the method is called, a new PrivateBuilder() instance is created, which
+* means there is no 1 to 1 ampping between the Built and PrivateBuilder instances. Consequently, there is no required double dependency injection
+* so each class can keep track of the other's existence.
+*
+* Create the PrivateBuilder class as a private class for the Built class. Then, make the AppendItem method private in the Built class, with
+* the PrivateBuilder's AddItem wrapping a call to AppendItem as the only way to construct the object in phases, enforcing hereby
+* the usage of the PrivateBuilder instance. Furthermore, for allowing chaining of the build phases, the Builder's AppendItem method shall 
+* return the instance of the Builder, for being reused. After the chained construction os ver, call () operator or ConstructObject() for
+* retrieving the built object.
+*
+* Additionallty, the GetBuilder() methods gets an overloaded template implementation with variadic args, which calls the Builder's template c-tor with
+* variadic args. This constructs the Built root object with the specificed args using perfect frowarding twice in a row.
 */
 
 class Built
@@ -18,25 +31,19 @@ class Built
     
     public:
     //c-tors
-    Built() : privateBuilderInstance{this}
-    {};
-    
+    Built() = default;
     ~Built() = default;
     
-    Built(const pair<string, string>& initValue) : privateBuilderInstance{this}
+    Built(const std::pair<std::string, std::string>& initValue)
     {
         mData.emplace(initValue);
     };
     
-    Built(const Built& input) : mData{input.mData},
-                                privateBuilderInstance{this}
-    {
-    };
+    Built(const Built& input) : mData{input.mData}
+    {};
     
-    Built(Built&& input) : mData{move(input.mData)},
-                           privateBuilderInstance{this}
-    {
-    };
+    Built(Built&& input) : mData{std::move(input.mData)}
+    {};
     
     Built& operator=(Built input)
     {
@@ -49,10 +56,10 @@ class Built
     }
     
     //setter and getter
-    map<string, string> GetData() const {return mData;};
-    void SetData(const map<string, string>& input)
+    std::map<std::string, std::string> GetData() const {return mData;};
+    void SetData(const std::map<std::string, std::string>& input)
     {
-        mData = move(input);
+        mData = std::move(input);
     }
     
     //other methods
@@ -60,46 +67,79 @@ class Built
     {
         for(auto&& keyVal : mData)
         {
-            cout<<keyVal.first<<" "<<keyVal.second<<endl;
+            std::cout<<keyVal.first<<" "<<keyVal.second<<std::endl;
         }
     }
     
-    PrivateBuilder& GetPrivateBuilderInstance()
+    static PrivateBuilder GetStaticPrivateBuilder()
     {
-        return privateBuilderInstance;
+        return PrivateBuilder();
+    }
+    
+    template<class ... Args>
+    static PrivateBuilder GetStaticPrivateBuilder(Args&& ... args)
+    {
+        return PrivateBuilder(std::forward<Args>(args)...);
     }
     
     private:
-    class PrivateBuilder
-    {
-        public:
-        PrivateBuilder() = default;
-        
-        PrivateBuilder(Built* root):rootObject{root}
-        {};
-        
-        //variadic template AddItem emthod which can comply with overloads for Appenditem
-        template<class ... Args>
-        void AddItem(Args&& ... args)
-        {
-            rootObject->AppendItem(std::forward<Args>(args)...);
-        }
-
-        //or use the dependency to insert data directly into the member map, bypassing the call to AppendItem
-        void AddItem(const pair<string, string>& item)
-        {
-            rootObject->mData.insert(item);
-        }
-        
-        private:
-        Built* rootObject{nullptr};
-    };
-    
-    void AppendItem(const pair<string, string>& item)
+    //Built members and private methods
+    void AppendItem(const std::pair<std::string, std::string>& item)
     {
         mData.insert(item);
     }
  
-    map<string, string> mData;
-    PrivateBuilder privateBuilderInstance;
+    std::map<std::string, std::string> mData;
+    
+    //Private builder class declaration and definition
+    class PrivateBuilder
+    {
+        public:
+        template<class ... Args>
+        PrivateBuilder(Args&& ... args)
+        {
+            if(!mUptrRootObject)
+            {
+                mUptrRootObject = std::make_unique<Built>(std::forward<Args>(args)...);
+            }
+        };
+        
+        PrivateBuilder()
+        {
+            if(!mUptrRootObject)
+            {
+                mUptrRootObject = std::make_unique<Built>();
+            }
+        };
+        
+        Built& operator()()
+        {
+            return *mUptrRootObject;
+        }
+        
+        Built& ConstructObject()
+        {
+            return *mUptrRootObject;
+        }
+        
+        //variadic template AddItem method which can comply with overloads for Appenditem
+        template<class ... Args>
+        PrivateBuilder& AddItem(Args&& ... args)
+        {
+            mUptrRootObject->AppendItem(std::forward<Args>(args)...);
+            
+            return *this;
+        }
+
+        //or use the dependency to insert data directly into the member map, bypassing the call to AppendItem
+        PrivateBuilder& AddItem(const std::pair<std::string, std::string>& item)
+        {
+            mUptrRootObject->mData.insert(item);
+            
+            return *this;
+        }
+        
+        private:
+        std::unique_ptr<Built> mUptrRootObject{nullptr};
+    };
 };
