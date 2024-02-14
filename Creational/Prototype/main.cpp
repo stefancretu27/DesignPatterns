b6326460aa0b166eca245af0de3ef58d8154eace
stefancretu27 <stefancretu27@gmail.com>
2024-02-14 16:30:11 +0200

    Update main.cpp

--- Creational/Prototype/main.cpp
+++ Creational/Prototype/main.cpp
@@ -4,6 +4,9 @@
 * Prototype is used to build complex objects from existing objects, by copying data from them, either fully or partially.
 * The fully data copy can be done via copy constructors that use deep copy approach.
 * The partial copy can leverage prototype data that is resued from an existing object and replicated for the newly created ones.
+*
+* Implementation-wise, in its simplest form, involves using a CreatWithPrototype method, which reuses the complex object
+* constructed for previous instances, or in previous operations, stored as member, and which can take additional input arguments.
 * 
 * After the creation of the new objects, they can be further customized, either by chaning some of the fully copied data, or
 * by adding values to the fields not set during partial copy process. Thus, this customization leads to the need to use
