39c1a134cac1b9f61dfbd5e2b94459a6ba01dc6c
stefancretu27 <stefancretu27@gmail.com>
2024-02-20 17:43:49 +0200

    Update main.cpp

--- Behavioral/Command/main.cpp
+++ Behavioral/Command/main.cpp
@@ -8,16 +8,30 @@
 *
 * Implementation-wise, the command class encapsulates definition of the operations it is able to do (as enum) and, eventually,
 * a flag that marks whther the operation succeeded or not. Additionally, it can hold state, as some data required by each
-* operation can be stored internally (as in amount of money involved in a transaction).
+* operation can be stored internally (as in amount of money involved in a transaction). Then, other classes can use this class
+* to process commands, taking appropriate decision for each command code, eventually storing state and using the value associated
+* with a certain command. Eventually, the command class/struct can be use via composition.
+*            ________________________________                        _____________________________________________
+*            |        struct Command -       |                       |             User class                    |
+*            |                               |                       |                                           |  
+*            | enum class CommandsCodes      |    Uses               | void ProcessCommand(Command& cmd)         |
+*            | { CMD1,                       |<----------------------| {                                         |
+*            |   CMD2,                       |                       |   switch(cmd)                             |
+*            |   CMD3} cmdsCodes;            |                       |   { case CommandsCodes::CMD1:             |  
+*            | bool flagOperationSuccessful; |                       |      cmd.flagOperationSuccessful==true;   |  
+*            | S valueUsedByOperation;       |                       |       break; }                            |   
+*            | T state;                      |                       |___________________________________________| 
+*            |_______________________________|
+*
 *
 * In some cases, the command class can store a reference member to class it operates on. As an alternative, a command instance
 * can be used by the class it operates on, either as a member or as method argument. It depends how it is desired to design
 * the business logic's control flow.
 *
 * There should be distinguished between a command and a query:
-*  - command: it causes the chyange of the object, but does not have a return value (much like a set operation), causing a 
+*  - command: it causes the change of the object, but does not have a return value (much like a set operation), causing a 
 * change in the system
-* - query: represents an interogation which has a return value, but which does not change the system (like a get operation) 
+*  - query: represents an interogation which has a return value, but which does not change the system (like a get operation) 
 */
 int main()
 {
