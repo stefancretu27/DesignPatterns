17188598a6df46533a32248deed46252be6b1b37
stefancretu27 <stefancretu27@gmail.com>
2024-02-21 16:45:00 +0200

    Update Observable.hpp

--- Behavioral/Observer/Observable.hpp
+++ Behavioral/Observer/Observable.hpp
@@ -3,54 +3,85 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <memory>
 
-template<class T>
-class Observer;
+//observer headers
+#include "Observer.hpp"
 
-template <class ObservableClass>
-class ObservableBase
+//Observed interface is a template whose type parameetrs include the Implementation class of this interface and the data type it works with
+//Exposes the required methods for add observer, remove observer, notify all observers, notify one observer, as wrappers of the private virtual methods
+template<class Impl, class DataType>
+class ObservableInterface
 {
-    private:
-    vector<Observer<ObservableClass>*> observers; 
-
     public:
-    void notify(ObservableClass& sourceObject, const string& fieldName)
+    void NotifyAll()
     {
-        for(Observer<ObservableClass>* obs : observers)
-        {
-            obs->fieldChanged(sourceObject, fieldName);
-        }
-    }
-
-    void subscribe(Observer<ObservableClass>& obs)
+        DoNotifyAll();
+    };
+    
+    void NotifyOne(const std::shared_ptr<ObserverInterface<Impl>>& observerObj)
     {
-        observers.push_back(&obs);
-    }
-
-    void unsubscribe(Observer<ObservableClass>& obs)
+        DoNotifyOne(observerObj);
+    };
+    
+    void Subscribe(const std::shared_ptr<ObserverInterface<Impl>>& observerObj)
     {
-        observers.erase( remove(observers.begin(),
-                                observers.end(),
-                                &obs),
-                         observers.end()
-                       );
-    }
+        DoSubscribe(observerObj);
+    };
+    
+    void Unsubscribe(const std::shared_ptr<ObserverInterface<Impl>>& observerObj)
+    {
+        DoUnsubscribe(observerObj);
+    };
+    
+    virtual DataType GetData() const = 0;
+    
+    virtual ~ObservableInterface() = default;
+    
+    private:
+    virtual void DoNotifyAll() = 0;
+    virtual void DoNotifyOne(const std::shared_ptr<ObserverInterface<Impl>>& observerObj) = 0;
+    virtual void DoSubscribe(const std::shared_ptr<ObserverInterface<Impl>>& observerObj) = 0;
+    virtual void DoUnsubscribe(const std::shared_ptr<ObserverInterface<Impl>>& observerObj) = 0;
 };
 
-// Observable class inherit from template Base class using CRTP idiom
-class MyObservableClass: public ObservableBase<MyObservableClass>
+//The implementation of the above Observable interface, pivately implementing the inherited virtual methods 
+//Keeps a list of observers, as shared_ptr due to copies required by std::algorithms, whilst envisaging to keep the outside created instances
+class ObservableClass : public ObservableInterface<ObservableClass, std::string>
 {
-    private:
-    int value;
-
     public:
-    MyObservableClass(const int val):value{val}{};
-
-    void setValue(const int val)
+    void SetData(const std::string& str){mData = str;};
+    std::string GetData() const {return mData;};
+    
+    private:
+    void DoNotifyAll() override
     {
-        value = val;
-        notify(*this, "value");
+        for(auto&& observer : mObserversList) 
+        {
+            observer->ProcessChange(*this);
+        }
     }
-
-    int getValue() const {return value;};
+    
+    void DoNotifyOne(const std::shared_ptr<ObserverInterface<ObservableClass>>& observerObj) override
+    {
+        observerObj->ProcessChange(*this);
+    }
+    
+    void DoSubscribe(const std::shared_ptr<ObserverInterface<ObservableClass>>& observerObj) override
+    {
+        auto it = std::find(mObserversList.begin(), mObserversList.end(), observerObj);
+        
+        if(it == mObserversList.end())
+        {
+            mObserversList.push_back(std::move(observerObj));
+        }
+    }
+    
+    void DoUnsubscribe(const std::shared_ptr<ObserverInterface<ObservableClass>>& observerObj) override
+    {
+        mObserversList.erase(std::remove(mObserversList.begin(), mObserversList.end(), observerObj), mObserversList.end());
+    }
+    
+    std::string mData;
+    std::vector<std::shared_ptr<ObserverInterface<ObservableClass>>> mObserversList;
 };
